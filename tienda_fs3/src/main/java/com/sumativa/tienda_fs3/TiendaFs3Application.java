5f99bdca446f490b04b52316b40da6752208dfc5
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2024-10-23 22:08:21 -0300

    se crea proyecto

---
+++ tienda_fs3/src/main/java/com/sumativa/tienda_fs3/TiendaFs3Application.java
@@ -0,0 +1,13 @@
+package com.sumativa.tienda_fs3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaFs3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaFs3Application.class, args);
+	}
+
+}
---
+++ tienda_fs3/src/test/java/com/sumativa/tienda_fs3/TiendaFs3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumativa.tienda_fs3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaFs3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
