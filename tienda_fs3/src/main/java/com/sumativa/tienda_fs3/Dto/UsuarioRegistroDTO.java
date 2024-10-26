70e30dcde6699c73f9845b632b4ff4c7bfcbae60
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2024-10-26 16:11:03 -0300

    se crea repository y DTO

---
+++ tienda_fs3/src/main/java/com/sumativa/tienda_fs3/Dto/UsuarioRegistroDTO.java
@@ -0,0 +1,81 @@
+package com.sumativa.tienda_fs3.Dto;
+
+public class UsuarioRegistroDTO {
+
+    private Long _id;
+    private String _nombre;
+    private String _apellido;
+    private String _direccion;
+    private String _email;
+    private String _password;
+
+    // constructor
+    public UsuarioRegistroDTO(Long _id, String _nombre, String _apellido, String _direccion, String _email,
+            String _password) {
+        this._id = _id;
+        this._nombre = _nombre;
+        this._apellido = _apellido;
+        this._direccion = _direccion;
+        this._email = _email;
+        this._password = _password;
+    }
+
+    // constructor email
+    public UsuarioRegistroDTO(String _email) {
+        this._email = _email;
+    }
+
+    // constructor vacio
+    public UsuarioRegistroDTO() {
+    }
+
+    // Getter and Setter
+    public Long get_id() {
+        return _id;
+    }
+
+    public void set_id(Long _id) {
+        this._id = _id;
+    }
+
+    public String get_nombre() {
+        return _nombre;
+    }
+
+    public void set_nombre(String _nombre) {
+        this._nombre = _nombre;
+    }
+
+    public String get_apellido() {
+        return _apellido;
+    }
+
+    public void set_apellido(String _apellido) {
+        this._apellido = _apellido;
+    }
+
+    public String get_direccion() {
+        return _direccion;
+    }
+
+    public void set_direccion(String _direccion) {
+        this._direccion = _direccion;
+    }
+
+    public String get_email() {
+        return _email;
+    }
+
+    public void set_email(String _email) {
+        this._email = _email;
+    }
+
+    public String get_password() {
+        return _password;
+    }
+
+    public void set_password(String _password) {
+        this._password = _password;
+    }
+
+}
---
+++ tienda_fs3/src/main/java/com/sumativa/tienda_fs3/Repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.sumativa.tienda_fs3.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sumativa.tienda_fs3.Model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
