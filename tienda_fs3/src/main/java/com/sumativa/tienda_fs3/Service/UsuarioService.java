aca321f5369f801c7db2154b15c453ffc2cacb36
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2024-10-26 21:03:22 -0300

    se configura seguridad de inicio de sesion

--- tienda_fs3/src/main/java/com/sumativa/tienda_fs3/Service/UsuarioService.java
+++ tienda_fs3/src/main/java/com/sumativa/tienda_fs3/Service/UsuarioService.java
@@ -1,10 +1,16 @@
 package com.sumativa.tienda_fs3.Service;
 
+import java.util.List;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.sumativa.tienda_fs3.Dto.UsuarioRegistroDTO;
 import com.sumativa.tienda_fs3.Model.Usuario;
 
-public interface UsuarioService {
+public interface UsuarioService extends UserDetailsService{
 
     public Usuario guardar (UsuarioRegistroDTO registroDTO);
+    
+    public List<Usuario> listarUsuarios();
     
 }
